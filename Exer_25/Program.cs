ff5d9288c8f4de1d805f42142d587f9743068748
Alina <alinakozlova908@gmail.com>
2022-07-10 22:34:03 +0300

    we add POWER!

---
+++ Exer_25/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter the first number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a power of this number: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+double RiseToPower(double a, double b)
+{
+    double result = 0;
+    
+    for (int i = 0; i<b; i++)
+    {
+        result = Math.Pow(a, b);
+       
+    }
+     Console.WriteLine(a + " to the " + b + " power is equal to " + result);
+     return result;
+}
+    
+RiseToPower(numberA, numberB);
